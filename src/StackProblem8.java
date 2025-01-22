613a6a052b5b6eb2e83e0626156f95664efc8971
JaydeepKhandla <jaydeepkhandla.work@gmail.com>
2025-01-22 14:39:35 +0530

    Add array printing utility and optimize largest rectangle area calculations in StackProblem8

--- src/StackProblem8.java
+++ src/StackProblem8.java
@@ -7,9 +7,20 @@ public class StackProblem8 {
         System.out.println(largestRectangleArea(heights));
     }
 
+    static void printArray(int[] arr) {
+        System.out.print("[ ");
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+        System.out.println("]");
+    }
+
     static int largestRectangleArea(int[] heights) {
         int[] left = findPrevSmaller(heights);
         int[] right = findNextSmaller(heights);
+
+        printArray(left);
+        printArray(right);
 
         int maxArea = 0;
 
@@ -27,7 +38,7 @@ public class StackProblem8 {
         int[] result = new int[arr.length];
 
         for (int i = 0; i < arr.length; i++) {
-            while (!stack.isEmpty() && stack.peek() >= arr[i]) {
+            while (!stack.isEmpty() && arr[stack.peek()] >= arr[i]) {
                 stack.pop();
             }
 
@@ -37,7 +48,7 @@ public class StackProblem8 {
                 result[i] = stack.peek();
             }
 
-            stack.push(arr[i]);
+            stack.push(i);
         }
 
         return result;
@@ -49,7 +60,7 @@ public class StackProblem8 {
         int[] result = new int[arr.length];
 
         for (int i = arr.length - 1; i >= 0; i--) {
-            while (!stack.isEmpty() && stack.peek() >= arr[i]) {
+            while (!stack.isEmpty() && arr[stack.peek()] >= arr[i]) {
                 stack.pop();
             }
 
@@ -59,10 +70,52 @@ public class StackProblem8 {
                 result[i] = stack.peek();
             }
 
-            stack.push(arr[i]);
+            stack.push(i);
         }
 
         return result;
     }
 
+    static int largestRectangleAreaV2(int[] heights) {
+        Stack<Integer> stack = new Stack<>();
+        int n = heights.length;
+        int maxArea = 0;
+
+        for (int i = 0; i <= n; i++) {
+            int h = (i == n ? 0 : heights[i]);
+
+            while (!stack.isEmpty() && h < heights[stack.peek()]) {
+                int tp = heights[stack.pop()];
+                int ps = stack.isEmpty() ? -1 : stack.peek();
+                maxArea = Math.max(maxArea, tp * (i - ps - 1));
+            }
+
+            stack.push(i);
+
+        }
+
+        return maxArea;
+    }
+
+    static int largestRectangleAreaV3(int[] heights) {
+        int[] stack = new int[heights.length];
+        int top = -1;
+        int n = heights.length;
+        int maxArea = 0;
+
+        for (int i = 0; i <= n; i++) {
+            int h = (i == n ? 0 : heights[i]);
+
+            while (top != -1 && h < heights[stack[top]]) {
+                int tp = heights[stack[top--]];
+                int ps = top == -1 ? -1 : stack[top];
+                maxArea = Math.max(maxArea, tp * (i - ps - 1));
+            }
+
+            stack[++top] = i;
+
+        }
+
+        return maxArea;
+    }
 }
